8e568e4e156794d0af268ddbae525465e7735453
Gladys Tanujaya <120657612+gladystanujaya17@users.noreply.github.com>
2024-06-15 18:32:01 +0700

    Time: 558 ms (98.97%), Space: 0B (100%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    employee_id,
+    CASE
+        WHEN name LIKE 'M%' THEN 0
+        WHEN employee_id % 2 = 0 THEN 0
+        ELSE salary
+    END AS bonus
+FROM Employees
+ORDER BY employee_id ASC;
