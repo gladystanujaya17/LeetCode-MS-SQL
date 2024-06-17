dd8cfff1c2abcaa1e6561d910fa592c4ab10cb1a
Gladys Tanujaya <120657612+gladystanujaya17@users.noreply.github.com>
2024-06-17 19:05:47 +0700

    Time: 1105 ms (86.73%), Space: 0B (100%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -4,4 +4,4 @@ SELECT
     name,
     mail
 FROM Users
-WHERE mail LIKE '[a-zA-Z]%@leetcode.com' AND LEFT(mail, LEN(mail) - 13) NOT LIKE '%[^0-9a-zA-Z_.-]%'
+WHERE mail LIKE '[a-zA-Z]%@leetcode.com' AND mail NOT LIKE '%[#%^$!&*()+=@]%@leetcode.com'
