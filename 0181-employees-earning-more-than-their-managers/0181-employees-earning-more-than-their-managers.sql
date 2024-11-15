df0e0fac2db80efb1deeb04bf7bb96d538ccacf6
Gladys Tanujaya <120657612+gladystanujaya17@users.noreply.github.com>
2024-11-15 15:24:35 +0700

    Time: 447 ms (92.67%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
-SELECT em2.name AS Employee
-FROM employee em2
-JOIN employee em1 ON em1.id = em2.managerId
-WHERE em1.salary < em2.salary;
+SELECT em.name AS Employee
+FROM employee em
+JOIN employee mgr ON mgr.id = em.managerId
+WHERE mgr.salary < em.salary;
